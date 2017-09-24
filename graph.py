6d67baedd7747f6503763b51613852a98b829abf
Max Alekhnovich <mla23@njit.edu>
2017-09-24 15:45:07 -0700

    methods related to adjacency matrix and graph theory

--- graph.py
+++ graph.py
@@ -42,3 +42,39 @@ class AdjacencyMatrixGraph(Graph):
             raise ValueError("An edge cannot have weight < 1")
 
         self.matrix[v1][v2] = weight
+
+        if self.directed ==False:
+            self.matrix[v2][v1]= weight
+
+    def get_adjacent_vertices(self,v):
+        if v < 0 or v >= self.numVertices:
+            raise ValueError("Cannot access vertex %d" %v)
+        adjacent_vertices = []
+        for i in range(self.numVertices):
+            #if vertex i is adjacent to v then it has
+            # 1 in the matrix[v][i] spot
+            if self.matrix[v][i] > 0:
+                adjacent_vertices.append(i)
+
+        return adjacent_vertices
+
+    #the number of edges directed into a vertex in a directed graph
+    def get_indegree(self,v):
+        #check for validity
+        if v < 0 or v>= self.numVertices:
+            raise ValueError("Cannot access vertex %d"%v)
+
+        indegree = 0
+        for i in range(self.numVertices):
+            if self.matrix[i][v]> 0 :
+                indegree = indegree + 1
+
+        return indegree
+
+    def get_edge_weight(self,v1,v2):
+        return self.matrix[v1][v2]
+
+    def display(self):
+        for i in range(self.numVertices):
+            for v in self.get_adjacent_vertices(i):
+                print(i +"---->"+ v)
