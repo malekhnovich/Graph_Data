30e44404236382ac6d33cf868ab869f689e4a895
Max Alekhnovich <mla23@njit.edu>
2017-09-24 16:26:43 -0700

    applying logic to run application with example

--- graph.py
+++ graph.py
@@ -78,3 +78,20 @@ class AdjacencyMatrixGraph(Graph):
         for i in range(self.numVertices):
             for v in self.get_adjacent_vertices(i):
                 print(i +"---->"+ v)
+
+
+#graph of 4 nodes
+g = AdjacencyMatrixGraph(4)
+g.add_edge(0,1)
+g.add_edge(0,2)
+g.add_edge(2,3)
+
+for i in range(4):
+    print ("Adjacent to: ", i, g.get_adjacent_vertices(i))
+
+for i in range(4):
+    print ("Indegree: ", i, g.get_indegree(i))
+
+for i in range(4):
+    for j in g.get_adjacent_vertices(i):
+        print("Edge weight: ",i," ",j," weight: ",g.get_edge_weight(i,j))
